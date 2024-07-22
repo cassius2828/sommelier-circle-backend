51d073877f8097e94015468d0b1d0b1ee11c7277
Cassius <cassius.reynolds.dev@gmail.com>
2024-07-22 09:04:39 -0700

    added route for photo details in place_id search

--- controllers/google-places.js
+++ controllers/google-places.js
@@ -1,11 +1,13 @@
 const axios = require("axios");
 
-const getNearbySearches = async (req,res) => {
+const GOOGLE_PLACES_BASE_URL = `https://maps.googleapis.com/maps/api/place`;
+
+const getNearbySearches = async (req, res) => {
   const { lat, lng, radius, type, keyword, key } = req.query;
 
   try {
     const response = await axios.get(
-      `https://maps.googleapis.com/maps/api/place/nearbysearch/json`,
+      `${GOOGLE_PLACES_BASE_URL}/nearbysearch/json`,
       {
         params: {
           location: `${lat},${lng}`,
@@ -22,6 +24,27 @@ const getNearbySearches = async (req,res) => {
   }
 };
 
+const getPhotoOfRoom = async (req, res) => {
+  const { photo_reference, key } = req.query;
+  try {
+    if (!photo_reference)
+      return res.status(400).json({ error: "No photo refernce was found" });
+    const response = await axios.get(
+      `${GOOGLE_PLACES_BASE_URL}/photo?maxwidth=400&
+      photo_reference=${photo_reference}
+      &key=${key}`
+    );
+    res.status(200).json(response.data)
+    // res.status(200).json({message: 'hit'})
+  } catch (err) {
+    res
+      .status(500)
+      .json({
+        error: "Cannot retrieve photos from google places photo_reference",
+      });
+  }
+};
+
 module.exports = {
-  getNearbySearches,
+  getNearbySearches,getPhotoOfRoom
 };
--- routes/google-places.js
+++ routes/google-places.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const googlePlacesCtrl = require("../controllers/google-places");
 
 router.get("/nearbysearch", googlePlacesCtrl.getNearbySearches);
+router.get("/room-photos", googlePlacesCtrl.getPhotoOfRoom);
 
 module.exports = router;
