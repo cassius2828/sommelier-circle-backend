7ad5266a958894e7b2037ee8c4e3b8c02ebcbf56
Cassius <cassius.reynolds.dev@gmail.com>
2024-07-22 21:41:12 -0700

    ensured google places api and photo api work on backend. Had to use 64encoded and buffers for the images as well as setting the content type to image/jpeg

--- controllers/google-places.js
+++ controllers/google-places.js
@@ -26,25 +26,29 @@ const getNearbySearches = async (req, res) => {
 
 const getPhotoOfRoom = async (req, res) => {
   const { photo_reference, key } = req.query;
+
   try {
-    if (!photo_reference)
-      return res.status(400).json({ error: "No photo refernce was found" });
-    const response = await axios.get(
-      `${GOOGLE_PLACES_BASE_URL}/photo?maxwidth=400&
-      photo_reference=${photo_reference}
-      &key=${key}`
-    );
-    res.status(200).json(response.data)
-    // res.status(200).json({message: 'hit'})
+    if (!photo_reference) {
+      return res.status(400).json({ error: "No photo reference was found" });
+    }
+
+    const url = `${GOOGLE_PLACES_BASE_URL}/photo?maxwidth=400&photo_reference=${photo_reference}&key=${key}`;
+    const response = await axios.get(url, { responseType: "arraybuffer" });
+
+    const base64Image = Buffer.from(response.data, "binary").toString("base64");
+    const mimeType = response.headers["content-type"];
+
+    res.status(200).json(`data:${mimeType};base64,${base64Image}`);
   } catch (err) {
-    res
-      .status(500)
-      .json({
-        error: "Cannot retrieve photos from google places photo_reference",
-      });
+    console.error(`Error fetching photo: ${err.message}`, err);
+    res.status(500).json({
+      error: "Cannot retrieve photos from Google Places photo_reference",
+      details: err.message,
+    });
   }
 };
 
 module.exports = {
-  getNearbySearches,getPhotoOfRoom
+  getNearbySearches,
+  getPhotoOfRoom,
 };
